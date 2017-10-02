7520a21db1b0802ae0f068bc9a7aa6b6436d3ad8
jbreton <jbreton@kronostechnologies.com>
2017-10-02 11:42:13 -0400

    Remove hard coded local url

--- Kronos/FileSystem/Mount/Local/Local.php
+++ Kronos/FileSystem/Mount/Local/Local.php
@@ -12,7 +12,12 @@ use League\Flysystem\Filesystem;
 class Local extends FlySystemBaseMount {
 
 	const MOUNT_TYPE = 'LOCAL';
-	const SIGNED_URL_BASE_PATH = 'utils/get_document.php?id=';
+	const ERR_MISSING_BASE_URL = 'Mount requires a base URL (setBaseUrl)';
+
+	/**
+	 * @var string
+	 */
+	protected $baseUrl;
 
 	/**
 	 * @param Filesystem $mount
@@ -23,12 +28,24 @@ class Local extends FlySystemBaseMount {
 	}
 
 	/**
+	 * @param string $baseUrl
+	 */
+	public function setBaseUrl($baseUrl) {
+		$this->baseUrl = $baseUrl;
+	}
+
+	/**
 	 * @param string $uuid
 	 * @param $fileName
 	 * @return string
+	 * @throws \Exception
 	 */
 	public function getSignedUrl($uuid, $fileName) {
-		return self::SIGNED_URL_BASE_PATH . $uuid;
+		if(!$this->baseUrl) {
+			throw new \Exception(self::ERR_MISSING_BASE_URL);
+		}
+
+		return $this->baseUrl . $uuid;
 	}
 
 	/**
